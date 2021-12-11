9ea926c6467dee5243ad18959b63ae6a77a1562f
Mark <gr_401@outlook.com>
2021-12-10 21:36:17 -0500

    Initial project

---
+++ HelloWorld/operators.py
@@ -0,0 +1,25 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)    # division produces a float result
+print(a // b)   # integer division, rounded down towards minus infinity
+print(a % b)    # module: the remainder after integer division
+
+print()
+
+for i in range(1, 4):
+    print(i)
+
+i = 1
+print(i)
+i = 2
+print(i)
+i = 3
+print(i)
+
+bun_price = 2.40
+money = 15
+print(money // bun_price)
