40272122cd7468dcf5978f5846b2639deba6e375
Mark <gr_401@outlook.com>
2021-12-27 14:52:32 -0500

    while loop

---
+++ FlowControl/escapechar.py
@@ -0,0 +1,17 @@
+print("The pet shop owner said \"No, no, 'e's uh, ... he's resting\".")
+
+print("""The pet shop owner said "No, no, \
+'e's uh,...he's resting".""")
+
+anotherString = """This string has been \
+split over \
+several \
+lines"""
+
+print(anotherString)
+
+print("C:\\Users\\mbuchan2\\notes.txt")
+print(r"C:\Users\mbuchan2\notes.txt")
+
+
+# find the three issues that do not conform to the pep8 style guide
--- FlowControl/guessinggame.py
+++ FlowControl/guessinggame.py
@@ -1,19 +1,55 @@
-answer = 5
-print("Please guess a number between 1 and 10: ")
+import random
+
+highest = 1000
+answer = random.randint(1, highest)
+print(answer)  # TODO: Remove after testing
+
+print("Please guess a number between 1 and {}: ".format(highest))
 guess = int(input())
-#  Challenge: rewrite program to check for correct answer first
-if guess == answer:
-    print("You got it the first time")
-else:
+# Use while loop to allow for multiple guesses
+while guess != answer:
+    if guess == 0:
+        print("Thanks for playing!")
+        break
     if guess < answer:
         print("Please guess higher")
     else:  # guess must be greater than answer
         print("Please guess lower")
     guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else:
-        print("Sorry, you have not guessed correctly")
+else:
+    print("Well done, you guessed it")
+
+#  Challenge: rewrite program to check for correct answer first
+# if guess == answer:
+#     print("You got it the first time")
+# else:
+#     if guess < answer:
+#         print("Please guess higher")
+#     else:  # guess must be greater than answer
+#         print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#        print("Sorry, you have not guessed correctly")
+
+#  Code before randomizing the answer.
+# answer = 5
+# print("Please guess a number between 1 and 10: ")
+# guess = int(input())
+# #  Challenge: rewrite program to check for correct answer first
+# if guess == answer:
+#     print("You got it the first time")
+# else:
+#     if guess < answer:
+#         print("Please guess higher")
+#     else:  # guess must be greater than answer
+#         print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
 
 
 #  Before Challenge
