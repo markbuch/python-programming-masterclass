3d322098f3c111a9287faba88701492f416e9d6a
Mark <gr_401@outlook.com>
2021-12-28 16:48:33 -0500

    Started working with sequences

---
+++ Sequences/lists_intro.py
@@ -0,0 +1,17 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]
+
+for part in computer_parts:
+    print(part)
+
+# Access by index
+print()
+print(computer_parts[2])
+
+# splice list
+print(computer_parts[0:3])  # produces another list
+print(computer_parts[-1])  # last item in the list
