2a2b47903e68ac7e4614967e095e3be00ef1564f
Mark Buchanan <markbuch24@outlook.com>
2021-12-30 09:39:43 -0500

    Working with lists

--- Sequences/number_lists.py
+++ Sequences/number_lists.py
@@ -1,13 +1,17 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-even.extend(odd)
-print(even)
-another_even = even
-print(another_even)
+# concatenate lists
+numbers = even + odd
+print(numbers)
 
-even.sort(reverse=True)
-print(even)
-print(another_even)
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+
+digits = sorted("432985617")
+print(digits)
+
 
 
