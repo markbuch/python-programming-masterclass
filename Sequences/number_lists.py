3c952d4279b1daa513d2221a22522244b04bc201
Mark Buchanan <markbuch24@outlook.com>
2022-01-02 20:19:43 -0500

    working with nested lists

--- Sequences/number_lists.py
+++ Sequences/number_lists.py
@@ -3,24 +3,15 @@ even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
 # concatenate lists
-numbers = even + odd
+numbers = [even, odd]
 print(numbers)
 
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-print(numbers)
+for number_list in numbers:
+    print(number_list)
 
-digits = list("432985617")
-print(digits)
+    for value in number_list:
+        print(value)
 
-# alternate ways to copy a list.
-# more_numbers = list(numbers)
-# more_numbers = numbers[:]
-more_numbers = numbers.copy()
-print(more_numbers)
-
-print(numbers is more_numbers)
-print(numbers == more_numbers)
 
 
 
---
+++ Sequences/spam.py
@@ -0,0 +1,9 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["egg", "bacon", "spam"],
+    ["egg", "bacon", "sausage", "spam"],
+    ["spam", "egg", "spam", "spam", "bacon", "spam"],
+    ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
+]
