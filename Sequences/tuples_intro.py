78167737f83bd85a8397414910405b9515091ff5
Mark Buchanan <markbuch24@outlook.com>
2022-01-03 21:22:03 -0500

    nested tuples

--- Sequences/tuples_intro.py
+++ Sequences/tuples_intro.py
@@ -1,16 +1,33 @@
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+# welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+# bad = "Bad Company", "Bad Company", 1974
+# budgie = "Nightflight", "Budgie", 1981
+# imelda = "More mayhem", "Emilda May", 2011
+# metallica = "Ride the Lightning", "Metallica", 1984
+#
+#
+# title, artist, year = metallica
+# print(title)
+# print(artist)
+# print(year)
+#
+# table = ("Coffee table", 200, 100, 75, 34.50)
+# print(table[1] * table[2])
+#
+# name, length, width, height, price = table
+# print(length * width)
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
 
-metallica2 = list(metallica)
-print(metallica2)
-metallica2[0] = "Master of Puppets"
-print(metallica2)
+# next example
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
+print(len(albums))
+for name, artist, year in albums:
+    print("Album: {}, Artist: {}. Year: {}"
+          .format(name, artist, year))
+
+
 
