ddb975114b742965e744aa2adde32acaa7beeacf
Mark Buchanan <markbuch24@outlook.com>
2022-01-22 18:27:20 -0500

    More dictionary work

--- DictAndSet/dict_intro.py
+++ DictAndSet/dict_intro.py
@@ -9,11 +9,40 @@ vehicles = {
 	'fiesta': 'Ford Fiesta Ghia 1.4',
 }
 
-my_car = vehicles['fiesta']
-print(my_car)
+# my_car = vehicles['fiesta']
+# print(my_car)
+#
+# commuter = vehicles['virago']
+# print(commuter)
+#
+# learner = vehicles.get("er5")
+# print(learner)
 
-commuter = vehicles['virago']
-print(commuter)
+# Assign key/value pairs to a dictinary.
+vehicles["starfigheter"] = "Lockheed F-104"
+vehicles["learjet"] = "Bombardier Learjet 75"
+vehicles["toy"] = "glider"
 
-learner = vehicles.get("er5")
-print(learner)
+# Upgrade the Virago (how to update a value in a dictionary)
+vehicles["virago"] = "Yamaha XV535"
+
+# delete a key/value from a dictionary
+del vehicles["starfigheter"]
+# del vehicles["f1"]
+
+# Better option to delete a key/value pair
+result = vehicles.pop("f1", "You wish! ")
+print(result)
+plane = vehicles.pop("learjet")
+print(plane)
+
+bike = vehicles.pop("tenere", "not present")
+print(bike)
+print()
+# how to iterate over a dictionary.
+# for key in vehicles:
+# 	print(key, vehicles[key], sep=", ")
+for key, value in vehicles.items():
+	print(key, value, sep=", ")
+
+
