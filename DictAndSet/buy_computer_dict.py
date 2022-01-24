5ffec40330db3dc3f5d9f27fd218123033914665
Mark Buchanan <markbuch24@outlook.com>
2022-01-24 11:20:36 -0500

    more work with dictionaries

--- DictAndSet/buy_computer_dict.py
+++ DictAndSet/buy_computer_dict.py
@@ -8,10 +8,19 @@ available_parts = {
 }
 
 current_choice = None
+computer_parts = {}  # create an empty dictionary
+
 while current_choice != "0":
     if current_choice in available_parts:
         chosen_part = available_parts[current_choice]
-        print(f"Adding {chosen_part}")
+        if current_choice in computer_parts:
+            # it's already in, so remove it
+            print(f"Removing {chosen_part}")
+            computer_parts.pop(current_choice)
+        else:
+            print(f"Adding {chosen_part}")
+            computer_parts[current_choice] = chosen_part
+        print(f"Your dictionary now contains: {computer_parts}")
     else:
         print("Please add options from the list")
         for key, value in available_parts.items():
--- DictAndSet/dict_list.py
+++ DictAndSet/dict_list.py
@@ -6,12 +6,18 @@ available_parts = {
     "5": "hdmi cable",
     "6": "dvd drive",
 }
-
+computer_parts = []
 current_choice = None
 while current_choice != "0":
     if current_choice in available_parts:
         chosen_part = available_parts[current_choice]
-        print(f"Adding {chosen_part}")
+        if chosen_part in computer_parts:
+            print(f"Removing {chosen_part}")
+            computer_parts.remove(chosen_part)
+        else:
+            print(f"Adding {chosen_part}")
+            computer_parts.append(chosen_part)
+        print(f"Your list now contains: {computer_parts}")
     else:
         print("Please add options from the list")
         for key, value in available_parts.items():
