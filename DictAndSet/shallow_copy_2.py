703f1632296e6edcd694ebc05aa5ccd087053949
Mark Buchanan <markbuch24@outlook.com>
2022-01-27 16:50:52 -0500

    shallow copy

--- DictAndSet/shallow_copy_2.py
+++ DictAndSet/shallow_copy_2.py
@@ -1,16 +1,29 @@
+lion_list = ["scary", "big", "cat"]
+elephant_list = ["big", "grey", "wrinkled"]
+teddy_list = ["cuddly", "stuffed"]
+
 animals = {
-	"lion": ["scary", "big", "cat"],
-	"elephant": ["big", "grey", "wrinkled"],
-	"teddy": ["cuddly", "stuffed"],
+	"lion": lion_list,
+	"elephant": elephant_list,
+	"teddy": teddy_list,
 }
 
-things = animals.copy()
+# things = animals.copy()
+things = {
+	"lion": lion_list,
+	"elephant": elephant_list,
+	"teddy": teddy_list,
+}
 
 print(things["teddy"])
 print(animals["teddy"])
 
 print()
 
-things["teddy"].append("toy")
+#  things["teddy"].append("toy")
+teddy_list.append("toy")
+animals["teddy"].append("added via `animals`")
+things["teddy"].append("added via `things`")
 print(things["teddy"])
 print(animals["teddy"])
+print(teddy_list)
