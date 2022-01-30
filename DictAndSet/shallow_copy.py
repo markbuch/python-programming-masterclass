ae93ec302d197847cc0b6295994ae7e4574d3054
Mark Buchanan <markbuch24@outlook.com>
2022-01-30 09:21:06 -0500

    Shallow and deep copy

--- DictAndSet/shallow_copy.py
+++ DictAndSet/shallow_copy.py
@@ -1,10 +1,16 @@
+import copy  # To use to perform a deep copy
+
 animals = {
 	"lion": ["scary", "big", "cat"],
 	"elephant": ["big", "grey", "wrinkled"],
 	"teddy": ["cuddly", "stuffed"],
 }
 
-things = animals.copy()
+# Perform a shallow copy
+# things = animals.copy()
+
+# Perform a deep copy
+things = copy.deepcopy(animals)
 
 print(things["teddy"])
 print(animals["teddy"])
